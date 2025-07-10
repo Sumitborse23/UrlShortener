e517c617abcb8014c89fe1e09180c2cdd888e83f
sumit <sumitborse23520@gmail.com>
2025-07-10 21:33:19 +0530

    Initial commit

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,24 @@
 import express from 'express';
 import { nanoid } from 'nanoid';
 import dotenv from 'dotenv';
-import connectDB from "./src/config/mongo.config.js"
+import connectDB from "./src/config/mongo.config.js";
 import shortUrl from './src/routes/shortUrl.route.js';
 import authRoutes from './src/routes/auth.route.js';
 import userRoutes from './src/routes/user.route.js';
-import {redirectFromShortUrl} from "./src/controller/shortUrl.controller.js"
+import { redirectFromShortUrl } from "./src/controller/shortUrl.controller.js";
 import { errorHandler } from './src/utils/errorHandler.js';
-import cors from 'cors'
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
 import { attachUser } from './src/utils/attachUser.js';
 
-dotenv.config("./.env");
+dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
-}))
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -30,12 +30,20 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/create", shortUrl)
-app.get("/:id",redirectFromShortUrl);
+app.use("/api/create", shortUrl);
+app.get("/:id", redirectFromShortUrl);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(3000, () => {
-  connectDB();
-  console.log('Server is running on http://localhost:3000');
-});
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to DB", err);
+    process.exit(1);
+  });
